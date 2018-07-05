ef390568691c1c146548ea2b3cf2d28b684c744e
Ibesoft11 <ibesoft11@gmail.com>
2018-07-05 09:56:14 +0100

    Added functions for managing comic books

---
+++ controllers/BooksController.js
@@ -0,0 +1,16 @@
+var model = require('../models/Book');
+//var service = require('../services/UserService');
+//const Joi = require('joi');
+
+exports.getBookByid = function(req, res){
+    var id = res.params.id;
+    model.findById(id, function(err, book){
+        if (err) res.json({err:err, message:'sorry, could not get book by id'});
+        res.json({message:book});
+    });
+}
+exports.getAllBooks = function(req, res){
+    model.find({}, '-__v', function(err, books){
+        if (err) res.json({err:err, message:'sorry, could not return all books'});
+    });
+}
