e6fb9e0728e3762ea8f55c1fef741785406b2adc
Ibesoft11 <ibesoft11@gmail.com>
2018-07-05 09:53:37 +0100

    Added Send mail, unsubscribe functions

---
+++ controllers/SubscriberController.js
@@ -0,0 +1,53 @@
+var model = require('../models/Subscriber');
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'djangelopoku@gmail.com',
+        pass: 'djangelonthebeats'
+    }
+  });
+
+exports.addSubscriber = function(req, res){
+    var data = {
+        email: req.body.email,
+        preferences: []
+    };
+    model.create(data, function(err, user){
+        if (err) res.json({err:err, message:'error occured while creating user'});
+        res.json({message:'Subscriber added successfully.'});
+    });
+}
+
+exports.deleteSubscriber = function(req, res){
+    var email = req.params.email;
+    model.remove({email:email}, function(err){
+        if (err) res.json({err:err, message:'could not delete subscriber'});
+        return res.json('user unsubscribed');
+    });
+}
+
+exports.sendNotification = function(req, res){
+    model.find({}, '-_id -preferences -__v', function(err, users){
+        if (err) res.json({err:err, message:'error while retrieving users'})
+        for (recipient of users){
+            var mailOptions = {
+                from: '"Privvy at Comic-Gallery"',
+                to: recipient,
+                subject: 'New Comic Updates',
+                html: '<h1>Hi there,</h1> <p>Hello from ComicGallery</p> <h1><i>TeamBoondocks</i></h1>'+
+                `<a href=\'http://comic-gallery/unsubscribe/${recipient['email']}\'>Unsubscribe</a>`
+              };
+              transporter.sendMail(mailOptions, function(error, info){
+                if (error) {
+                  console.log(error);
+                } else {
+                  console.log('Email sent: ' + info.response);
+                }
+              });
+        }
+    });
+    return res.json('notification sent');
+}
+//
+//
