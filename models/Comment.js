61cfcefc20dec182c2f517fd41509f40c3adfd7e
Ndife <stilesndife@gmail.com>
2018-07-06 08:34:38 +0100

    made changes to the comment schema

--- models/Comment.js
+++ models/Comment.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 var CommentSchema = mongoose.Schema({
    time: Date,
    commentBody: String,
-   person: String
+   person: String,
+    book: {type: mongoose.Schema.Types.ObjectId, ref: 'Book'}
 });
 
 module.exports = mongoose.model('Comment', SubscriberSchema);
